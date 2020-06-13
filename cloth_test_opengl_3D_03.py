5afee2f6b90322990afb252fd9a2ca2bcb4ea766
TANUKIpro <ryotaro_koike@outlook.com>
2020-06-14 02:48:16 +0900

    カメラビューの移植完了

--- cloth_test_opengl_3D_03.py
+++ cloth_test_opengl_3D_03.py
@@ -7,17 +7,19 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore as Qt
-from PyQt5.QtCore    import *
+from PyQt5.QtCore import *
 from PyQt5.QtOpenGL import *
 from stl import mesh
 from stl_load_class import STL_loader
 from drawer_class import drawPolygon, drawText, drawText_3D, drawAxis
 import ctypes
 
+window_title = "Cloth SIM@PyQt5 v.0.3"
 screen_size = [700, 600]
 
 to_models = "/Users/ryotaro/py_projects/pygame_sim/model"
 finger = "/Index"
+
 names_list = ["/Metacarpal3_01.stl",
               "/Proximal_Phalanx3_01.stl",
               "/Middle_Phalanxh3_01.stl",
@@ -52,10 +54,11 @@ class QTGLWidget2(QGLWidget):
                            [tuple((0, 0, 0)), tuple((0, 0, 5))]]
         self.br = 0.0
         self.bg = 0.0
-        self.bb = 1.0
+        self.bb = 0.0
         self.Meta, self.PrxPh, self.MddPh, self.DisPh = False, False, False, False
         self.keys_list = []
-        self.set_AxisViw_and_CameraAngle()
+        self.all_camera_status = []
+
 
     def listener(self, bool_list):
         self.Meta, self.PrxPh, self.MddPh, self.DisPh = bool_list
@@ -64,39 +67,49 @@ class QTGLWidget2(QGLWidget):
         key = event.key()
         move_pix = 0.5
         if event.modifiers() & Qt.ShiftModifier:
-            if   key==Qt.Key_X : self.camera_cood[0][0] -= 0.1
-            elif key==Qt.Key_Y : self.camera_cood[1][0] -= 0.1
-            elif key==Qt.Key_Z : self.camera_cood[2][0] -= 0.1
+            if   key==Qt.Key_X : self.camera_cood[0][0] -= 0.05
+            elif key==Qt.Key_Y : self.camera_cood[1][0] -= 0.05
+            elif key==Qt.Key_Z : self.camera_cood[2][0] -= 0.05
 
             elif key == Qt.Key_Up : self.angle_x += move_pix
             elif key == Qt.Key_Down : self.angle_x -= move_pix
 
-        elif key==Qt.Key_X : self.camera_cood[0][0] += 0.1
-        elif key==Qt.Key_Y : self.camera_cood[1][0] += 0.1
-        elif key==Qt.Key_Z : self.camera_cood[2][0] += 0.1
+        elif key==Qt.Key_X : self.camera_cood[0][0] += 0.05
+        elif key==Qt.Key_Y : self.camera_cood[1][0] += 0.05
+        elif key==Qt.Key_Z : self.camera_cood[2][0] += 0.05
 
         elif key==Qt.Key_Left  : self.angle_y += move_pix
         elif key==Qt.Key_Right : self.angle_y -= move_pix
         elif key==Qt.Key_Up    : self.angle_z += move_pix
         elif key==Qt.Key_Down  : self.angle_z -= move_pix
 
-    def mouse_L_Down(self):
-        pass
+    def cameraRESET(self):
+        self.camera_rot = [70,23]
+        self.camera_radius = 2.5
+        self.camera_center = [0.5,0.5,0.5]
+        self.camera_cood = [[0.],[0.],[0.]]
+        self.angle_x, self.angle_y, self.angle_z = 0., 0., 0.
+        self.vias_x, self.vias_y, self.vias_z = 0.,0.,0.
 
-    def set_AxisViw_and_CameraAngle(self, org_point=False, axis=False):
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        glViewport(0,0,screen_size[0],screen_size[1])
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        gluPerspective(self.camera_wide_angle,float(screen_size[0])/float(screen_size[1]), 0.1, 100.0)
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
+    def mouse_listener(self, type, event, mv_cood=[0, 0]):
+        ## LEFT, MIDDLE, RIGHT, WHEEL, MOVE
+        move_pix = 0.5
+        if type == 'MOVE':
+            self.camera_rot[0] += mv_cood[0]
+            self.camera_rot[1] += mv_cood[1]
+            #self.update()
+        if type == 'WHEEL':
+            if   event.angleDelta().y() == 120  : self.camera_radius -= move_pix
+            elif event.angleDelta().y() == -120 : self.camera_radius += move_pix
 
+    def paintGL(self):
+        glClearColor(self.br, self.bg, self.bb, 0.0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        glLoadIdentity()
         camera_pos = [self.camera_center[0] + self.camera_radius*cos(radians(self.camera_rot[0]))*cos(radians(self.camera_rot[1])),
                       self.camera_center[1] + self.camera_radius*sin(radians(self.camera_rot[1])),
                       self.camera_center[2] + self.camera_radius*sin(radians(self.camera_rot[0]))*cos(radians(self.camera_rot[1]))]
-
-        ####  gluLookAt(視点の位置(x,y,z), 目標の位置(x,y,z), 法線ベクトル(,,))
         gluLookAt(camera_pos[0], camera_pos[1], camera_pos[2],
                   self.camera_center[0], self.camera_center[1], self.camera_center[2], 0,1,0)
 
@@ -107,27 +120,16 @@ class QTGLWidget2(QGLWidget):
         ## 指定軸に対して平行移動
         glTranslatef(-self.camera_cood[0][0], -self.camera_cood[1][0], -self.camera_cood[2][0])
 
-        if org_point:
-            glColor3f(1, 1, 0)
-            glPointSize(15)
-            glBegin(GL_POINTS)
-            glVertex3fv(self.org)
-            glEnd()
-        if axis:
-            drawText_3D("X", 5., 0., 0.)
-            drawText_3D("Y", 0., 5., 0.)
-            drawText_3D("Z", 0., 0., 5.)
-            drawAxis()
-        self.updateGL()
+        glColor3f(1, 1, 0)
+        glPointSize(30)
+        glBegin(GL_POINTS)
+        glVertex3fv(self.org)
+        glEnd()
 
-    def paintGL(self):
-        glClearColor(self.br, self.bg, self.bb, 0.0)
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-        #glLoadIdentity()
-        #gluLookAt(3.0, 4.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
-        print(self.camera_cood)
-        #drawText(self.camera_cood.encode('utf-8'), 5, screen_size[1]-15, *screen_size)
+        drawText_3D("X", 3., 0., 0.)
+        drawText_3D("Y", 0., 3., 0.)
+        drawText_3D("Z", 0., 0., 3.)
+        drawAxis()
 
         if not self.Meta:
             for idx_Meta in Metacarpal3:
@@ -142,8 +144,8 @@ class QTGLWidget2(QGLWidget):
             for idx_DisPh in Distal_Phalanxh3:
                 drawPolygon(idx_DisPh)
         glFlush()
-        self.updateGL()
-        
+        #self.updateGL()
+
     def resizeGL(self, w, h):
         glViewport(0, 0, w, h)
         glMatrixMode(GL_PROJECTION)
@@ -159,11 +161,12 @@ class QTGLWidget2(QGLWidget):
 
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        gluPerspective(40.0, 1.0, 1.0, 30.0)
+        gluPerspective(40.0, 1.0, .1, 100.0)
 
 class QTWidget(QWidget):
     def __init__(self):
         QWidget.__init__(self)
+        self.clicked_points = [0, 0]
         self.bool_list = [True, True, True, True]
         self.listCheckBox = names_list
         self.listLabel = []
@@ -171,6 +174,11 @@ class QTWidget(QWidget):
             self.listLabel.append("")
         self.gl = QTGLWidget2(self)
         self.initUI()
+
+        timer = QTimer(self)
+        timer.setInterval(20)   # period, in milliseconds
+        timer.timeout.connect(self.gl.updateGL)
+        timer.start()
 
     def initUI(self):
         gui_layout = QGridLayout()
@@ -183,10 +191,14 @@ class QTWidget(QWidget):
             gui_layout.addWidget(self.listCheckBox[i], i+10, 0)
             gui_layout.addWidget(self.listLabel[i],    i+10, 1)
 
-        self.button = QPushButton("Clear")
-        self.button.clicked.connect(self.check_checkbox)
+        self.sc_button = QPushButton("Show / Clear")
+        self.sc_button.clicked.connect(self.check_checkbox)
         # (ウェジット、行、列)
-        gui_layout.addWidget(self.button, 20, 0)
+        gui_layout.addWidget(self.sc_button, 20, 0)
+
+        self.rst_button = QPushButton("RESER CAMERA VIEW")
+        self.rst_button.clicked.connect(self.gl.cameraRESET)
+        gui_layout.addWidget(self.rst_button, 30, 0)
 
     def check_checkbox(self):
         for i, v in enumerate(self.listCheckBox):
@@ -199,35 +211,36 @@ class QTWidget(QWidget):
     def keyPressEvent(self, event):
         self.gl.key_listener(event)
 
+    ## LEFT, MIDDLE, RIGHT, WHEEL, MOVE
     def mouseButtonKind(self, buttons):
-        if buttons & Qt.LeftButton:
-            print("LEFT")
-        if buttons & Qt.MidButton:
-            print("MIDDLE")
-        if buttons & Qt.RightButton:
-            print("RIGHT")
+        if buttons & Qt.LeftButton  : self.gl.mouse_listener("LEFT",   None)
+        if buttons & Qt.MidButton   : self.gl.mouse_listener("MIDDLE", None)
+        if buttons & Qt.RightButton : self.gl.mouse_listener("RIGHT",  None)
 
     def mousePressEvent(self, e):
-        print("BUTTON PRESS")
         self.mouseButtonKind(e.buttons())
+        self.clicked_points = [e.pos().x(), e.pos().y()]
 
     def mouseReleaseEvent(self, e):
-        print("BUTTON RELEASE")
         self.mouseButtonKind(e.buttons())
 
     def wheelEvent(self, e):
-        print("wheel")
-        print("(%d %d)" % (e.angleDelta().x(), e.angleDelta().y()))
+        self.gl.mouse_listener("WHEEL", e)
 
     def mouseMoveEvent(self, e):
-        print("(%d %d)" % (e.x(), e.y()))
+        # マウスの相対移動座標
+        mvX, mvY = self.clicked_points[0]-e.x(), self.clicked_points[1]-e.y()
+        self.gl.mouse_listener("MOVE", e, mv_cood=[-mvX*0.2, -mvY*0.2])
+        self.update()
+
+def main():
+    app = QApplication(sys.argv)
+    w = QTWidget()
+    w.setWindowTitle(window_title)
+    w.show()
+
+    sys.exit(app.exec_())
+
 
 if __name__=='__main__':
-    #try:
-        app = QApplication(sys.argv)
-        w = QTWidget()
-        w.setWindowTitle('PyQt OpenGL 2')
-        w.show()
-    #except:
-        print("Error")
-        sys.exit(app.exec_())
+    main()
