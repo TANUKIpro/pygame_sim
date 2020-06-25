4fbdcc5aadf4c7fd7d1bc41eea75fb3fb9325098
TANUKIpro <ryotaro_koike@outlook.com>
2020-06-25 21:51:02 +0900

    計算精度を許容するための工夫(あまりうまく動かない)

--- dxf_loader.py
+++ dxf_loader.py
@@ -1,29 +1,32 @@
 import numpy as np
 import dxfgrabber
 from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
+from math import floor
 
 #file_name="model/extensor_hood_test001.dxf"
 #file_name="model/extensor_hood_test002.dxf"
 
 class DXF_Loader:
-    def __init__(self, file_name, extensor_reduced_scale, x_vias, y_vias, z_vias, inversion=1):
+    def __init__(self, file_name, extensor_reduced_scale, x_vias, y_vias, z_vias, inversion=1, integer=False):
         self.file_name = file_name
-        self.extensor_reduced_scale = extensor_reduced_scale
+        self.reduced_scale = extensor_reduced_scale
         self.x_vias = x_vias#-100
         self.y_vias = y_vias#880
         self.z_vias = z_vias
         self.inversion = inversion  ## 反転操作(-1or1)
-        self.round_quality = 6
+        self.round_quality = 5
 
         self.stop_points = []      ## 固定パーティクルの座標が格納
         self.poly_lines = []       ## ポリラインの頂点座標が格納
         self.particle_points = []  ## パーティクルの座標が格納
+        self.integer = integer
         self.load_dxf()
 
     def get_unique_list(self, seq):
         seen=[];return [x for x in seq if x not in seen and not seen.append(x)]
 
     def load_dxf(self):
+        n=12
         dxf = dxfgrabber.readfile(self.file_name)
         for i, layer in enumerate(dxf.layers):
             print("Layer {0} : {1}".format(i, layer.name))
@@ -33,27 +36,36 @@ class DXF_Loader:
         all_particle_points_en  = [e for e in dxf.entities if e.layer == 'particle_points'] ##  only CIRCLE
 
         for circle in all_stop_points_en:
-            #x = int(round(circle.center[0] * self.extensor_reduced_scale + self.x_vias, 1))
-            #y = int(round(circle.center[1] * self.extensor_reduced_scale * self.inversion + self.y_vias, 1))
-            x = round(circle.center[0] * self.extensor_reduced_scale + self.x_vias, self.round_quality)
-            y = round(circle.center[1] * self.extensor_reduced_scale * self.inversion + self.y_vias, self.round_quality)
+            if not self.integer:
+                x = floor((circle.center[0] * self.reduced_scale + self.x_vias)*10**n) / (10**n)
+                y = floor((circle.center[1] * self.reduced_scale * self.inversion + self.y_vias)*10**n) / (10**n)
+                x, y = round(x, self.round_quality), round(y, self.round_quality)
+            else:
+                x = int(circle.center[0] * self.reduced_scale + self.x_vias)
+                y = int(circle.center[1] * self.reduced_scale * self.inversion + self.y_vias)
             self.stop_points.append([x, y])
 
         for lw_polyline in all_polly_lines_en:
             lump = []
             for cood in lw_polyline.points:
-                #x = int(round(cood[0] * self.extensor_reduced_scale + self.x_vias, 1))
-                #y = int(round(cood[1] * self.extensor_reduced_scale * self.inversion + self.y_vias, 1))
-                x = round(cood[0] * self.extensor_reduced_scale + self.x_vias, self.round_quality)
-                y = round(cood[1] * self.extensor_reduced_scale * self.inversion + self.y_vias, self.round_quality)
+                if not self.integer:
+                    x = floor((cood[0] * self.reduced_scale + self.x_vias)*10**n) / (10**n)
+                    y = floor((cood[1] * self.reduced_scale * self.inversion + self.y_vias)*10**n) / (10**n)
+                    x, y = round(x, self.round_quality), round(y, self.round_quality)
+                else:
+                    x = int(cood[0] * self.reduced_scale + self.x_vias)
+                    y = int(cood[1] * self.reduced_scale * self.inversion + self.y_vias)
                 lump.append([x, y])
             self.poly_lines.append(lump)
 
         for circle in all_particle_points_en:
-            #x = int(round(circle.center[0] * self.extensor_reduced_scale + self.x_vias, 1))
-            #y = int(round(circle.center[1] * self.extensor_reduced_scale * self.inversion + self.y_vias, 1))
-            x = round(circle.center[0] * self.extensor_reduced_scale + self.x_vias, self.round_quality)
-            y = round(circle.center[1] * self.extensor_reduced_scale * self.inversion + self.y_vias, self.round_quality)
+            if not self.integer:
+                x = floor((circle.center[0] * self.reduced_scale + self.x_vias)*10**n) / (10**n)
+                y = floor((circle.center[1] * self.reduced_scale * self.inversion + self.y_vias)*10**n) / (10**n)
+                x, y = round(x, self.round_quality), round(y, self.round_quality)
+            else:
+                x = int(circle.center[0] * self.reduced_scale + self.x_vias)
+                y = int(circle.center[1] * self.reduced_scale * self.inversion + self.y_vias)
             self.particle_points.append([x, y])
 
         print("Clearing duplicate particles : ", len(self.particle_points), "-->",
@@ -61,7 +73,6 @@ class DXF_Loader:
         self.particle_points = self.get_unique_list(self.particle_points)
 
     def ver_col_ind(self):
-        #ver, col, ind = [], [], []
         """  2次元座標 --> 3次元座標　へ変換  """
         stop_points_2d = np.array(self.stop_points)
         stop_points_3d = np.zeros((stop_points_2d.shape[0], 3))+self.z_vias
@@ -81,23 +92,6 @@ class DXF_Loader:
             poly_lines_3d.append(np.hstack((line_clump, z_zeros)))
 
         return stop_points_3d, particle_points_3d, poly_lines_3d
-
-        """
-        ## 3次元座標を1次元に変換 (STOP POINTS)
-        sp_num, sp_corner = stop_points_3d.shape
-        sp_3d  = stop_points_3d.reshape([sp_num*sp_corner, 1])
-        ver_sp = np.zeros((sp_num*sp_corner, 1)).tolist()
-        for i, sp in enumerate(sp_3d):
-            ver_sp[i] = sp[0]
-
-        ## 3次元座標を1次元に変換 (PARTICLE POINTS)
-        pp_num, pp_corner = particle_points_3d.shape
-        pp_3d  = particle_points_3d.reshape([pp_num*pp_corner, 1])
-        ver_pp = np.zeros((pp_num*pp_corner, 1)).tolist()
-        for i, pp in enumerate(pp_3d):
-            ver_pp[i] = pp[0]
-        """
-
 
     def color(self, ver, _r=1, _g=1, _b=1):
         col = np.zeros_like(ver).tolist()
